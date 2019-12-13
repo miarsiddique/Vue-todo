50bcd9ec28f485b6b4d0ca1827b807bc499262a0
jyoti-aika <irshad.alam@logiciesolutions.co.in>
2019-12-13 12:44:34 +0530

    vue todo added

---
+++ src/components/TodoList.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <input type="test" class="todo-list" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
+    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+      <div>
+        {{todo.title}}
+      </div>
+      <div class="remove-item" @click="removeTodo(index)">
+        &times;
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+  data () {
+    return {
+      newTodo: '',
+      idForTodo: 3,
+      todos: [
+        {
+          'id': 1,
+          'title': 'Finish vue screen cast',
+          'completed': false,
+        },
+        {
+          'id': 2,
+          'title': 'Take new task for project',
+          'completed': false,
+        }
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+
+      if(this.newTodo.trim().length == 0) {
+        return;
+      }
+
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false,
+      });
+
+      this.newTodo = '';
+      this.idForTodo++;
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  .todo-list{
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+
+    &:focus {
+      outline: 0;
+    }
+  }
+  .todo-item {
+    margin-bottom: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .remove-item {
+    cursor: pointer;
+    margin-left: 14px;
+    &: hover {
+      color: black;
+    }
+  }
+</style>
